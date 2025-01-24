c8cf5bc5968cf6c0828eaa85b760592881d770dd
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-01-24 11:53:12 +0100

    Add parseRouteString utility and extend WeatherRepository interface

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ import { Aerodrome, ReportingPoint, Waypoint } from "./airport";
 import { MetarData } from "./metar";
 import { AerodromeService, WeatherService } from "./service";
 import { RouteLeg, RouteOptions, routePlan, RouteTrip } from "./planner";
-
-/**
- * The version of the library.
- */
-export const LIBRARY_VERSION = "1.0.1";
+import { parseRouteString } from "./utils";
 
 /**
  * Represents a weather station with METAR and optional TAF information.
@@ -24,6 +20,8 @@ export interface MetarStation {
  */
 export interface WeatherRepository {
   findByICAO(icao: string): Promise<MetarStation | undefined>;
+  refreshByRadius(location: GeoJSON.Point, radius: number): Promise<void>;
+  nearestStation(location: GeoJSON.Point, radius: number, exclude: string[]): Promise<MetarStation | undefined>;
 }
 
 /**
@@ -33,7 +31,7 @@ export interface AerodromeRepository {
   findByICAO(icao: string): Promise<Aerodrome | undefined>;
 }
 
-export { parseMetar };
+export { parseMetar, parseRouteString };
 export { Waypoint, ReportingPoint, Aerodrome };
 export { WeatherService, AerodromeService };
 export { RouteOptions, RouteLeg, RouteTrip, routePlan }
