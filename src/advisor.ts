1bb05baf1bad591d4f6b0fee4db4934b39bae60b
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-05-21 17:19:06 +0200

    Add additional safety checks and warnings to advisor.ts

--- src/advisor.ts
+++ src/advisor.ts
@@ -8,3 +8,5 @@
 // - Check weight & balance
 // - Minimum Safe Altitude (SERA.5005.f / NCO.OP.145): Enroute altitude > 500 ft
 // - Minimum Safe Altitude (SERA.5005.f / NCO.OP.145): Enroute altitude > 1000 ft above highest obstacle within 500 M
+// - Low temparature warning
+// - Flight plan for controlled airspace
