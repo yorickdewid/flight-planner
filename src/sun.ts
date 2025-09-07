5305aeb9758ac727b02a48d8caed9b6266f6a2b8
Yorick de Wid <yorickdewid@users.noreply.github.com>
2025-09-07 09:37:10 +0000

    Refactor sun event calculations to accept both Waypoint and Position types

--- src/sun.ts
+++ src/sun.ts
@@ -1,41 +1,43 @@
 import suncalc, { GetTimesResult } from 'suncalc';
 import { Waypoint } from './waypoint.types.js';
+import type { Position } from 'geojson';
 
 /**
  * Calculates sun events for a given waypoint and date.
  *
- * @param waypoint - The waypoint for which to calculate sun events
+ * @param location - The waypoint or geographic position (longitude, latitude)
  * @param date - The date for which to calculate sun events
  * @returns An object containing sun event times
  */
-export const calculateSunEvents = (waypoint: Waypoint, date: Date = new Date()): GetTimesResult => {
-  const longitude = waypoint.location.geometry.coordinates[0];
-  const latitude = waypoint.location.geometry.coordinates[1];
+export const calculateSunEvents = (location: Waypoint | Position, date: Date = new Date()): GetTimesResult => {
+  const coordinates = 'location' in location ? location.location.geometry.coordinates : location;
+  const longitude = coordinates[0];
+  const latitude = coordinates[1];
 
   return suncalc.getTimes(date, latitude, longitude);
 }
 
 /**
- * Determines if a given time is during daylight at the specified waypoint.
+ * Determines if a given time is during daylight at the specified location.
  *
- * @param waypoint - The waypoint to check for daylight
+ * @param location - The waypoint or geographic position to check for daylight
  * @param time - The time to check (defaults to current time)
  * @returns true if the specified time is during daylight (between sunrise and sunset)
  */
-export const isDaylight = (waypoint: Waypoint, time: Date = new Date()): boolean => {
-  const events = calculateSunEvents(waypoint, time);
+export const isDaylight = (location: Waypoint | Position, time: Date = new Date()): boolean => {
+  const events = calculateSunEvents(location, time);
   return time >= events.sunrise && time <= events.sunset;
 }
 
 /**
- * Determines if a given time is during night at the specified waypoint.
+ * Determines if a given time is during night at the specified location.
  * Night is defined as the period between the end of evening civil twilight and the beginning of morning civil twilight.
  *
- * @param waypoint - The waypoint to check for night conditions
+ * @param location - The waypoint or geographic position to check for night conditions
  * @param time - The time to check (defaults to current time)
  * @returns true if the specified time is during night
  */
-export const isNight = (waypoint: Waypoint, time: Date = new Date()): boolean => {
-  const events = calculateSunEvents(waypoint, time);
+export const isNight = (location: Waypoint | Position, time: Date = new Date()): boolean => {
+  const events = calculateSunEvents(location, time);
   return time <= events.dawn || time >= events.dusk;
 }
